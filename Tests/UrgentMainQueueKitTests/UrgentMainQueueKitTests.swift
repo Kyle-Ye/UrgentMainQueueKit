30992c63b53a7b2134a6795d5e34314b2a6bde33
Kyle <kyle201817146@gmail.com>
2025-07-12 20:54:01 +0800

    Update example

--- Tests/UrgentMainQueueKitTests/UrgentMainQueueKitTests.swift
+++ Tests/UrgentMainQueueKitTests/UrgentMainQueueKitTests.swift
@@ -39,4 +39,34 @@ struct UrgentMainQueueKitTests {
         // ...
         // Greeting from 499
     }
+
+    @Test
+    func withHookInMainThread2() {
+        UMQ_AddMainQueueObserverHook()
+        DispatchQueue.main.async {
+            print("1: Current main queue task begin")
+            DispatchQueue.main.async {
+                print("3: Non-Urgent task execute")
+            }
+            // cut in line and execute first after the current main queue task if it is not main thread otherwise execute immediately
+            UMQ_AddUrgentMainQueueTasks {
+                print("4: Urgent task execute")
+            }
+            print("1. Current main queue task end")
+        }
+        DispatchQueue.main.async {
+            print("2: Normal task")
+        }
+
+        UMQ_RemoveMainQueueObserverHook()
+        // Expected output
+        // 1. Current main queue task begin
+        // 4. Urgent task execute
+        // 1. Current main queue task end
+        // 2. Normal task
+        // 3. Non-Urgent task execute
+
+        // If we replace `UMQ_AddUrgentMainQueueTasks` with `DispatchQueue.main.async`, the output will be:
+        // 1 2 3 4
+    }
 }
